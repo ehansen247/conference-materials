533ce995af96a752ce94be40109ef86dfc0aa8af
Eric Hansen <erichansen@college.harvard.edu>
2019-05-02 21:38:13 -0400

    Changed cloud-intro -> serverless -> app.py

--- cloud-intro/serverless/app.py
+++ cloud-intro/serverless/app.py
@@ -8,6 +8,12 @@ def index():
     return {'hello': 'world'}
 
 
-@app.route('/cool',cors=True)
+@app.route('/uncertainty', cors=True)
 def index():
-    return {'which_endpoint_is_the_coolest?': 'this endpoint is the coolest !!!'}
+    return {
+                "Is this a JSON Object?": "I'm not sure",
+                "Let's test it out!": "Ok",
+                "It sure seems like it": "Yeah it seems to be...",
+                "Let's join HCS to find out!": "Sounds like a plan!",
+                "Creds to": "Eric Hansen"
+            }
